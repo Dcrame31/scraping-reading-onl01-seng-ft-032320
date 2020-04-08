ea8453b8f87f60b92d1faf218e634599f6e05bb9
Diane Crame <dcrame@icloud.com>
2020-04-08 16:31:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
 
-doc =Nokogiri::HTML(html)
+
+doc =Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headine-260IBN")
 
 puts doc
